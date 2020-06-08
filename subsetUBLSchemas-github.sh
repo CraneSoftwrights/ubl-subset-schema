f6ff7f285fc077cfe3df63f32cd228a6c14b540a
gkholman <gkenholman+github@gmail.com>
2020-06-08 10:24:14 -0400

    Rearrange raw files

--- subsetUBLSchemas-github.sh
+++ subsetUBLSchemas-github.sh
@@ -2,4 +2,4 @@
 #
 # This is the invocation that happens in the GitHub action ... it must be bash
 #
-bash subsetUBLSchemas-DBEcoreDemo.sh "$1" "$2" "$3" "$4"
+bash subsetUBLSchemas-UBL-2.1-Demo.sh "$1" "$2" "$3" "$4"
