4caea8b4b3c681067170aff243c1916acbd67a2b
gkholman <gkenholman+github@gmail.com>
2020-06-08 16:22:03 -0400

    Demo 2.3 shortcut

--- subsetUBLSchemas-github.sh
+++ subsetUBLSchemas-github.sh
@@ -2,4 +2,4 @@
 #
 # This is the invocation that happens in the GitHub action ... it must be bash
 #
-bash subsetUBLSchemas-DemoUBL21Subset.sh "$1" "$2" "$3" "$4"
+bash subsetUBLSchemas-DemoUBL23CSPRD02Subset.sh "$1" "$2" "$3" "$4"
